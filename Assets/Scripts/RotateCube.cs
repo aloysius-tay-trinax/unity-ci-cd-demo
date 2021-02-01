434ad5da8c9b7b3efc25509f85ae64ebc7bc569d
aloysius-tay-trinax <62983290+aloysius-tay-trinax@users.noreply.github.com>
2021-02-02 03:16:19 +0800

    Initial commit

---
+++ Assets/Scripts/RotateCube.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCube : MonoBehaviour
+{
+    Transform _t;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _t = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _t.Rotate(Vector3.one * 50.0f * Time.deltaTime);
+    }
+}
---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+# Changelog
+
+## [0.0.1] - 2-02-2021
+
+### Added
+
+- Initial commit files
